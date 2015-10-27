334baa8e8a9183201a59946416a581996bb6f152
Luke Lancaster <luketlancaster@gmail.com>
2015-10-27 13:54:47 -0500

    play around with standard views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   namespace :api, path: '/', constraints: { subdomain: 'api' } do
     resources :stores
   end
+  resources :stores, only: [:index, :new, :create, :edit, :update]
 end
